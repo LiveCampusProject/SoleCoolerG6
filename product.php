d93a162f964ffdfdc2a1661260d664889b77c29a
Lucky Marty <contact@luckymarty.fr>
2023-02-07 10:03:16 +0100

    Missing Index Footer / Add GDPR

---
+++ product.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+     *  On définie le path de tout les fichiers nécéssaires et le titre de notre page 
+     *  Puis on insère les élements redondants footer nav et header 
+     */
+
+$js = "";
+$css = "./css/main.css";
+$titre = "SoleCooler";
+$bodyId = "index";
+require_once("./inc/head.php");
+require_once("./inc/header.php");
+?>
+
+<main>
+
+</main>
+
+<?php
+require_once("./inc/footer.php");
+?>
