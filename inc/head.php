53dcbe25c042d28a08ebc1c6328d3e66e1c0521f
Lucky Marty <contact@luckymarty.fr>
2023-02-09 16:48:58 +0100

    Favicon

--- inc/head.php
+++ inc/head.php
@@ -1,13 +1,13 @@
 <!-- Entête html du site -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- On charge le css du footer présent sur toute les pages -->
-    <link rel="stylesheet" href=/css/footer.css>
+
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon/favicon.ico">
     <!-- On charge le script de traduction présent sur toute les pages -->
     <script type="module" src="js/index.js"></script>
     <!-- On vérifie si la page que l'on charge à besoin d'un fichier css ou d'un ficher js -->
