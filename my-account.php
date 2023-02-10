bb57fadfe3ac0d6d928b525bf240b286ec070391
Lucky Marty <contact@luckymarty.fr>
2023-02-10 10:39:04 +0100

    Add My Account Page - Done

--- my-account.php
+++ my-account.php
@@ -17,6 +17,25 @@ require_once("./inc/header.php");
 <main>
     <div class="container space-arround-m">
         <h1>Mon compte</h1>
+
+        <div>
+            <div class="intro">
+                <img src="./assets/exemple/user.jpg" alt="img">
+                <div class="name">Bonjour ...</div>
+            </div>
+
+            <div class="block-1">
+                <a href="#">Gestion de compte</a>
+                <a href="#">Suivre ma commande</a>
+                <a href="#">Mes commandes</a>
+                <a href="#">Aides</a>
+            </div>
+
+            <div class="block-2">
+                <a href="./logout.php">Déconnexion</a>
+                <a href="#">Changer de compte</a>
+            </div>
+        </div>
     </div>
 </main>
 
